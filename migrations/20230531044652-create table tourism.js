11c0a11ed79f3e8bcc65dc645cac87aa7c363ac6
Aziz Rosyid <azizrosyid783@gmail.com>
2023-05-31 11:58:32 +0700

    feat: create table tourism

---
+++ migrations/20230531044652-create table tourism.js
@@ -0,0 +1,61 @@
+"use strict";
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable("tourism", {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      place_name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      description: {
+        type: Sequelize.TEXT,
+        allowNull: false,
+      },
+      category_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: "Categories",
+          key: "id",
+        },
+      },
+      city: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      price: {
+        type: Sequelize.DECIMAL(10, 2),
+        allowNull: false,
+      },
+      rating: {
+        type: Sequelize.FLOAT,
+        allowNull: true,
+      },
+      lat: {
+        type: Sequelize.FLOAT,
+        allowNull: true,
+      },
+      lng: {
+        type: Sequelize.FLOAT,
+        allowNull: true,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable("tourism");
+  },
+};

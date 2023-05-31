d4535133c7fdd89fa31fca9a44e066a513f8e720
Aziz Rosyid <azizrosyid783@gmail.com>
2023-05-31 12:00:49 +0700

    feat: create model tourism

---
+++ src/models/tourism.js
@@ -0,0 +1,59 @@
+"use strict";
+const { Model } = require("sequelize");
+
+module.exports = (sequelize, DataTypes) => {
+  class Tourism extends Model {
+    static associate(models) {
+      Tourism.belongsTo(models.Category, { foreignKey: "category_id" });
+    }
+  }
+  Tourism.init(
+    {
+      place_name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      description: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+      },
+      category_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      city: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      price: {
+        type: DataTypes.DECIMAL(10, 2),
+        allowNull: false,
+      },
+      rating: {
+        type: DataTypes.FLOAT,
+        allowNull: true,
+      },
+      lat: {
+        type: DataTypes.FLOAT,
+        allowNull: true,
+      },
+      lng: {
+        type: DataTypes.FLOAT,
+        allowNull: true,
+      },
+      createdAt: {
+        allowNull: false,
+        type: DataTypes.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: DataTypes.DATE,
+      },
+    },
+    {
+      sequelize,
+      modelName: "Tourism",
+    }
+  );
+  return Tourism;
+};

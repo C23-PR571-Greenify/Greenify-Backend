254959c1936937db21f6b3c9051a3634e796843e
Aziz Rosyid <azizrosyid783@gmail.com>
2023-05-31 11:57:54 +0700

    feat: import route category

--- src/app.js
+++ src/app.js
@@ -1,22 +1,24 @@
-const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
-const cors = require('cors');
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const cors = require("cors");
 
-const userRouter = require('./app/user/route');
-const authRouter = require('./app/auth/route');
+const userRouter = require("./app/user/route");
+const authRouter = require("./app/auth/route");
+const categoryRouter = require("./app/category/route");
 
 const app = express();
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/users', userRouter);
-app.use('/auth', authRouter);
+app.use("/users", userRouter);
+app.use("/auth", authRouter);
+app.use("/categories", categoryRouter);
 
 module.exports = app;

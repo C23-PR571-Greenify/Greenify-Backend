37ab1fd09c91e84330a5ca64d247e964695e700f
Aziz Rosyid <azizrosyid783@gmail.com>
2023-06-09 15:19:37 +0700

    feat: add api recommendation

--- src/app/tourism/handler.js
+++ src/app/tourism/handler.js
@@ -1,5 +1,7 @@
+const { default: axios } = require("axios");
 const { Tourism, Categories } = require("../../models");
 const { respone } = require("../../utils/response");
+const fs = require("fs");
 
 async function getAllTourismHandler(req, res, next) {
   try {
@@ -70,10 +72,43 @@ async function deleteTourismHandler(req, res, next) {
   }
 }
 
+async function predictTourismHandler(req, res, next) {
+  try {
+    const tourism = await Tourism.findAll();
+
+    const rating = req.body.rating;
+
+    const body = {
+      user_feature: [rating],
+      tempat_feature: tourism.map((item) => [
+        item.rating,
+        item.price,
+        item.lat,
+        item.lng,
+        item.category_id,
+      ]),
+    };
+
+    const recommendation = await axios.post(
+      `https://greenify-predict-ernafpm2wq-as.a.run.app/prediksi`,
+      body
+    );
+
+    const resultTourism = recommendation.data.map((item) => tourism[item]);
+
+    res
+      .status(200)
+      .json(respone("Berhasil mendapatkan rekomendasi", resultTourism));
+  } catch (error) {
+    next(error);
+  }
+}
+
 module.exports = {
   getAllTourismHandler,
   getSingleTourismHandler,
   createTourismHandler,
   updateTourismHandler,
   deleteTourismHandler,
+  predictTourismHandler,
 };
--- src/app/tourism/route.js
+++ src/app/tourism/route.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const handler = require("./handler");
 
+router.post("/recommendation", handler.predictTourismHandler);
 router.get("/", handler.getAllTourismHandler);
 router.get("/:id", handler.getSingleTourismHandler);
 router.post("/", handler.createTourismHandler);
